0916ef976181a5704272e4768b5b203559032bc8
jyung17 <iotaxixi@gmail.com>
2022-12-01 00:46:19 +0900

    Feat: 스프링 핵심원리 이해1 - 예제만들기 회원 도메인 설계, 개발, 회원 도메인 실행과 테스트

---
+++ src/main/java/hello/core/member/Member.java
@@ -0,0 +1,35 @@
+package hello.core.member;
+
+
+public class Member {
+  private Long id;
+  private String name;
+  private Grade grade;
+  
+  public Member(Long id, String name, Grade grade) {
+    this.id = id;
+    this.name = name;
+    this.grade = grade;
+  }
+  
+  @Override
+  public String toString() {
+    return "Member{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", grade=" + grade +
+        '}';
+  }
+  
+  public Long getId() {
+    return id;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public Grade getGrade() {
+    return grade;
+  }
+}
