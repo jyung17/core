c3882c3c2634eec072e9ba3d6f5c09bd7ff477fa
jyung17 <iotaxixi@gmail.com>
2022-12-08 04:54:58 +0900

    .

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,19 +1,25 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor // final이 붙은것을 가지고 생성자를 자동으로 생성해준다.
 public class OrderServiceImpl implements OrderService {
   
   private final MemberRepository memberRepository;
   private final DiscountPolicy discountPolicy;
   
+  
   @Autowired
-  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+  public OrderServiceImpl(MemberRepository memberRepository,
+      @MainDiscountPolicy
+      DiscountPolicy discountPolicy) {
     this.memberRepository = memberRepository;
     this.discountPolicy = discountPolicy;
   }
